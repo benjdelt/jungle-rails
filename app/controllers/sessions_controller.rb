60884bfa5bb3f7333b91c4b059bd9beb1b8a4043
Ben Deltenre <benjdelt@gmail.com>
2018-09-12 22:16:23 +0000

    Fixed login bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
-      session[:user_id] = user_id
+
+      session[:user_id] = user.id
       redirect_to '/'
     else  
       flash[:notice] = "Failed to log in"
