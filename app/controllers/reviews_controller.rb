d0a800991d615636fd2764e063d9a302cb6ad378
Ben Deltenre <benjdelt@gmail.com>
2018-09-14 00:50:24 +0000

    Added form handling method

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,15 @@
+class ReviewsController < ApplicationController
+
+  def create
+    @review = Review.new(review_params)
+    @review.user_id = current_user.id
+    @review.product_id = params[:product_id]
+    @review.save!
+    redirect_to Product.find params[:product_id]
+  end
+  
+  def review_params 
+    params.require(:review).permit(:rating, :description, :product_id)
+  end 
+ 
+end
