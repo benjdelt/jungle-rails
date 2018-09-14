00be5f76c3e49f10630b6b1c9bab36998173616c
Ben Deltenre <benjdelt@gmail.com>
2018-09-14 18:47:31 +0000

    Added delete button

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,17 @@
 class ReviewsController < ApplicationController
+  before_filter :authorize
 
   def create
     @review = Review.new(review_params)
     @review.user_id = current_user.id
     @review.product_id = params[:product_id]
     @review.save!
+    redirect_to Product.find params[:product_id]
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
     redirect_to Product.find params[:product_id]
   end
   
