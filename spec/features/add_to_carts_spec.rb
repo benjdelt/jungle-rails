c7ffaa379fd1e04718ac8f371fad70403e34400c
Ben Deltenre <benjdelt@gmail.com>
2018-09-18 04:04:32 +0000

    Added add to cart feature specs

---
+++ spec/features/add_to_carts_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "The cart is updated when they click Add to cart" do
+    visit root_path
+
+
+
+    first('.btn').click
+
+
+    # sleep 2
+
+
+
+    expect(page).to have_content('My Cart (1)')
+
+  end
+
+end 
